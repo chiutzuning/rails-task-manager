679e5b6c340bdaa81b462d9748b6ba944a552857
Tzuning Chiu <chiutzuning@gmail.com>
2022-02-15 21:17:38 +0100

    add some note

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Read (all), I can list tasks
+  get "tasks", to: "tasks#index"
+  # Create, I can add a new task
+  get "tasks/new", to: "tasks#new"
+  # POST the form
+  post "tasks", to: "tasks#create"
+  # Read (one), I can view the details of a task
+  get "tasks/:id", to: "tasks#show", as: :task
+  # Update, I can edit a task (mark as completed / update title & details)
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+  # Delete, I can remove a task
+  delete "tasks/:id", to: "tasks#destroy"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_15_104241) do
+ActiveRecord::Schema.define(version: 2022_02_15_105634) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
